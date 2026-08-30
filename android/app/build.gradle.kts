1a895ad2c6fbc5b17c1b8b6b483f4a0508bf8a62
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-30 09:10:00 -0300

    fix(android): preserve CMake DSL while adding tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
     externalNativeBuild {
         cmake {
             path("src/main/cpp/CMakeLists.txt")
-            version("3.31.6")
+            version = "3.31.6"
         }
     }
 
